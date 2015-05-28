9d9cec6b4a53b520191e16a9448087728be0b1ea
Justin Cormack <justin@specialbusservice.com>
2015-05-28 21:31:49 +0000

    permissions on img

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -x
 tar cf root.tar etc data
 rump.tar c -f - /dev > dev.tar
 dd if=/dev/zero of=fs.img bs=1k count=10k
+chmod a+rw fs.img
 rexec rump.newfs fs.img -- /dev/rblock0
 cat dev.tar | rexec rump.dd fs.img -- of=dev.tar
 cat root.tar | rexec rump.dd fs.img -- of=root.tar
