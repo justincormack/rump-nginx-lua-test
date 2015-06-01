93f0cafb3b84f5be66b4df9a7a483f6f8f7123b5
Justin Cormack <justin@specialbusservice.com>
2015-06-01 13:46:45 +0000

    work around docker build running as root

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ tar cf root.tar etc data
 rump.tar c -f - /dev > dev.tar
 dd if=/dev/zero of=fs.img bs=1k count=10k
 chmod a+rw fs.img
-rexec rump.newfs fs.img -- /dev/rblock0
-cat dev.tar | rexec rump.dd fs.img -- of=dev.tar
-cat root.tar | rexec rump.dd fs.img -- of=root.tar
-rexec rump.tar fs.img -- xf dev.tar
-rexec rump.tar fs.img -- xf root.tar
-rexec rump.rm fs.img -- dev.tar root.tar
+rexec rump.newfs -u 1000 fs.img -- /dev/rblock0
+cat dev.tar | rexec rump.dd -u 1000 fs.img -- of=dev.tar
+cat root.tar | rexec rump.dd -u 1000 fs.img -- of=root.tar
+rexec rump.tar -u 1000 fs.img -- xf dev.tar
+rexec rump.tar -u 1000 fs.img -- xf root.tar
+rexec rump.rm -u 1000 fs.img -- dev.tar root.tar
