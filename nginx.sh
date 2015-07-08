c69ab59e743534812fc1de5a60f55a8940dcf938
Justin Cormack <justin@specialbusservice.com>
2015-07-08 13:46:34 +0000

    cleanup

--- nginx.sh
+++ nginx.sh
@@ -7,13 +7,11 @@ die()
 }
 
 IFACE="eth0"
+
 export FIXED_ADDRESS=`ip addr show dev $IFACE | grep "inet " | awk '{print $2}' | cut -f1 -d/`
 export FIXED_MASK=`ip addr show dev $IFACE | grep "inet " | awk '{print $2}' | cut -f2 -d/`
 export FIXED_GATEWAY=`ip route get 8.8.8.8 | grep via | cut -f3 -d ' '`
 
 ip addr del $FIXED_ADDRESS/$FIXED_MASK dev $IFACE || die "need cap NET_ADMIN"
 
-echo $FIXED_ADDRESS/$FIXED_MASK $FIXED_GATEWAY
-INTERFACE="packet:$IFACE"
-
-rexec nginx -nx -ro fs.img -rw ${INTERFACE} -- -c /data/conf/nginx.conf
+rexec nginx -nx -ro fs.img -rw "packet:${IFACE}" -- -c /data/conf/nginx.conf
